53f191a55c32825bf6becda17b41da022e16a63f
4Rohit7 <rtalsaniya74@gmail.com>
2025-02-25 00:01:33 +0530

    Complete Backend

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -10,6 +10,7 @@ import {
   sendVerificationEmail,
   sendWelcomeEmail,
   sendPasswordResetEmail,
+  sendResetSuccessEmail,
 } from '../mailtrap/emails.js';
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
@@ -146,5 +147,51 @@ export const forgotPassword = async (req, res) => {
   } catch (error) {
     console.log('Error in forgotPassword', error);
     res.status(400).json({ success: false, message: error.message });
+  }
+};
+
+export const resetPassword = async (req, res) => {
+  try {
+    const { token } = req.params;
+    const { password } = req.body;
+
+    const user = await User.findOne({
+      resetPasswordToken: token,
+      resetPasswordExpiredAt: { $gt: Date.now() },
+    });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'Invalid or expired reset token' });
+    }
+    //update Password
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    user.password = hashedPassword;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpiredAt = undefined;
+    await user.save();
+
+    await sendResetSuccessEmail(user.email);
+    res
+      .status(200)
+      .json({ success: true, message: 'Password reset successful' });
+  } catch (error) {
+    console.log('Error in resetPassword', error);
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+
+export const checkAuth = async (req, res) => {
+  try {
+    const user = await User.findById(req.userId).select('-password');
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'User not found' });
+    }
+    res.status(200).json({ success: true, user });
+  } catch (error) {
+    console.log('Error in checkAuth', error);
+    res.status(400).json({ success: false, ressage: error.message });
   }
 };
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { connectDB } from './db/connectDB.js';
 import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 const PORT = process.env.PORT || 5000;
 
 app.use('/api/auth', authRoutes);
